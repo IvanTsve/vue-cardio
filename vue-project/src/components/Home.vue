9e331fde569550df5c0fa6664ba6203736c3af86
IvanTsve <iv_plamenovv@abv.bg>
2022-04-24 15:35:10 +0300

    added comment text area

--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -2,8 +2,13 @@
 import { onMounted, ref } from 'vue';
 import getData from '../scripts/fetch';
 import endpoints from '../scripts/endpoints';
+import mockDB from '../../mockDB/comments.json';
 
 const data = ref([]);
+const comment = ref('');
+const anon = ref(true);
+const postid = ref(null);
+
 onMounted(async () => {
     let options = {
         method: 'GET',
@@ -13,9 +18,19 @@ onMounted(async () => {
             'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
         }
     };
-     data.value = await getData(endpoints.news, options);
+    data.value = await getData(endpoints.news, options);
 })
 
+function onAddComment(e) {
+    const data = {
+        comment: comment.value,
+        anon: anon.value,
+        postid: e.target.id
+    };
+    mockDB.comments.push(data)
+    console.log(mockDB);
+
+}
 
 </script>
 
@@ -28,7 +43,15 @@ onMounted(async () => {
             <img v-if="article.image" :src="article.image.thumbnail.contentUrl" alt="article img" />
             <img v-else src="../assets/not-found-image-15383864787lu.jpg" alt="article img" />
             <p>{{ article.description }}</p>
-            <button>Add comment</button>
+            <form @submit.prevent="onAddComment" :id="id">
+                <textarea v-model="comment" :id="id"></textarea>
+                <div>
+                    <input type="checkbox" :id="id" v-model="anon"/>
+                    <label :for="id" >Anon</label>
+                </div>
+                <button>Add comment</button>
+            </form>
+
         </li>
     </ul>
 
